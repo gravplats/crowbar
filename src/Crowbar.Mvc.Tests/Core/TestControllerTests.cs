491fc8eb4741341181ef4739bfb0d54c13172fac
Mattias Rydengren <mattias.rydengren@coderesque.com>
2012-09-05 19:38:03 +0200

    Added sanity check for performing a request against an unknown resource.

--- src/Crowbar.Mvc.Tests/Core/TestControllerTests.cs
+++ src/Crowbar.Mvc.Tests/Core/TestControllerTests.cs
@@ -99,5 +99,18 @@ namespace Crowbar.Mvc.Tests.Core
                 }
             });
         }
+
+        [TestCase("DELETE")]
+        [TestCase("GET")]
+        [TestCase("POST")]
+        [TestCase("PUT")]
+        public void Should_return_http_not_found_when_performing_request_against_an_unknown_path(string method)
+        {
+            Server.Execute((context, browser) =>
+            {
+                var response = browser.PerformRequest(method, "/unknown");
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            });
+        }
     }
 }
