cdfb3830ea7d937ff7c69f19a7564441b16b89cc
Mattias Rydengren <mattias.rydengren@coderesque.com>
2012-09-06 17:32:20 +0200

    Updated default settings for DefaultDocumentStoreBuilder.

--- src/Crowbar/DefaultDocumentStoreBuilder.cs
+++ src/Crowbar/DefaultDocumentStoreBuilder.cs
@@ -2,15 +2,33 @@ using System;
 using Crowbar.Mvc;
 using Raven.Client;
 using Raven.Client.Embedded;
+using Raven.Client.Listeners;
 
 namespace Crowbar
 {
     [Serializable]
     public class DefaultDocumentStoreBuilder : IDocumentStoreBuilder
     {
+        public class WaitForNonStaleResultsListener : IDocumentQueryListener
+        {
+            public void BeforeQueryExecuted(IDocumentQueryCustomization customization)
+            {
+                customization.WaitForNonStaleResults();
+            }
+        }
+
         public IDocumentStore Build()
         {
-            return new EmbeddableDocumentStore { RunInMemory = true }.Initialize();
+            var store = new EmbeddableDocumentStore
+            {
+                Configuration =
+                {
+                    RunInMemory = true,
+                    RunInUnreliableYetFastModeThatIsNotSuitableForProduction = true
+                }
+            };
+
+            return store.RegisterListener(new WaitForNonStaleResultsListener()).Initialize();
         }
     }
 }
