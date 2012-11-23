03480db60241993a9417eccf81e51b84fb4bab56
Mattias Rydengren <mattias.rydengren@coderesque.com>
2012-11-23 20:29:43 +0100

    Merged two classes with extension methods for BrowserContext.

--- src/Crowbar/BrowserContextExtensions.cs
+++ src/Crowbar/BrowserContextExtensions.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Security;
+using Crowbar.AspNetMvc;
 
 namespace Crowbar
 {
@@ -16,6 +18,42 @@ namespace Crowbar
         public static void AjaxRequest(this BrowserContext context)
         {
             context.Header("X-Requested-With", "XMLHttpRequest");
+        }
+
+        /// <summary>
+        /// Adds an anti-forgery request token to the request.
+        /// </summary>
+        /// <param name="context">The <see cref="BrowserContext"/> that this data should be added to.</param>
+        /// <param name="username">The username.</param>
+        /// <param name="salt">The salt string.</param>
+        /// <param name="domain">The domain of the web application that the request is submitted from.</param>
+        /// <param name="path">The virtual root path of the web application that the request is submitted from.</param>
+        /// <param name="applicationPath">The application path.</param>
+        public static void AntiForgeryRequestToken(this BrowserContext context, string username = "", string salt = "", string domain = null, string path = null, string applicationPath = "/")
+        {
+            var serializer = new AntiForgeryDataSerializer();
+
+            var cookieToken = AntiForgeryData.NewToken();
+            string cookieValue = serializer.Serialize(cookieToken);
+
+            string cookieName = AntiForgeryData.GetAntiForgeryTokenName(applicationPath);
+            var cookie = new HttpCookie(cookieName, cookieValue) { Domain = null, HttpOnly = true };
+
+            if (!String.IsNullOrEmpty(path))
+            {
+                cookie.Path = path;
+            }
+
+            context.Cookie(cookie);
+
+            var formToken = new AntiForgeryData(cookieToken)
+            {
+                Salt = salt,
+                Username = username
+            };
+
+            string formValue = serializer.Serialize(formToken);
+            context.FormValue(AntiForgeryData.AntiForgeryTokenFieldName, formValue);
         }
 
         /// <summary>
