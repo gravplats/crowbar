8fd8c84ba2f22073df9dd94e4d802d7a51b17384
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-01-12 17:29:07 +0100

    Updated build script: added a nuspec file instead of an auto-generated one.

--- default.ps1
+++ default.ps1
@@ -2,15 +2,20 @@
 include .\extensions.ps1
 
 properties {
-    $path              = resolve-path .
-    $buildDirectory    = "$path\build"
-	$binariesDirectory = "$buildDirectory\binaries\"
-    $nugetDirectory    = "$buildDirectory\nuget\"
-    $releaseDirectory  = "$buildDirectory\release\"
-    $sourceDirectory   = "$path\src"
-    $solution          = "$sourceDirectory\Crowbar.sln"	
-    $toolsDirectory    = "$path\tools\"
-    $version           = "0.9"
+    $path            = resolve-path .
+    
+	$path_build      = "$path\build"
+	$path_binaries   = "$path_build\binaries\"
+    $path_nuget      = "$path_build\nuget\"
+    $path_release    = "$path_build\release\"
+	$nuspec          = "$path_nuget\Crowbar.nuspec"
+    
+	$path_source     = "$path\src"
+    $solution        = "$path_source\Crowbar.sln"	
+    
+	$path_tools      = "$path\tools\"
+	
+    $version         = "0.9"
 }
 
 $framework = '4.0'
@@ -18,51 +23,48 @@ $framework = '4.0'
 task default -depends release
 
 task clean {
-    Remove-Item -Force -Recurse -ErrorAction SilentlyContinue -LiteralPath $buildDirectory
+    Remove-Item -Force -Recurse -ErrorAction SilentlyContinue -LiteralPath $path_build
 }
 
 task build -depends clean {
-    New-Item $binariesDirectory -ItemType Directory | Out-Null
+    New-Item $path_binaries -ItemType Directory | Out-Null
 
     Generate-Assembly-Info `
-        -file "$sourceDirectory\Crowbar\Properties\AssemblyInfo.cs" `
+        -file "$path_source\Crowbar\Properties\AssemblyInfo.cs" `
         -title "Crowbar $version" `
         -description "Application testing for ASP.NET MVC 3 and 4" `
         -product "Crowbar" `
         -version $version `
         -copyright "Copyright (C) Mattias Rydengren 2012-2013" `
 
-    msbuild $solution /p:OutDir=$binariesDirectory /p:Configuration=Release | Out-Null
+    msbuild $solution /p:OutDir=$path_binaries /p:Configuration=Release | Out-Null
 }
 
 task release -depends build {
-    New-Item $releaseDirectory -ItemType Directory | Out-Null
+    New-Item $path_release -ItemType Directory | Out-Null
 
-    & $toolsDirectory\7-zip\7za.exe a $releaseDirectory\crowbar-$version-release-net-4.0.zip `
-        $binariesDirectory\Crowbar.dll `
-        $binariesDirectory\Crowbar.pdb `
-        $binariesDirectory\Crowbar.xml `
+    & $path_tools\7-zip\7za.exe a $path_release\crowbar-$version-release-net-4.0.zip `
+        $path_binaries\Crowbar.dll `
+        $path_binaries\Crowbar.pdb `
+        $path_binaries\Crowbar.xml `
         LICENSE | Out-Null
 }
 
 task nuget -depends release {
-    New-Item $nugetDirectory\lib\net40 -ItemType Directory | Out-Null
-        
-    Generate-NuGet-Spec `
-        -file "$nugetDirectory\Crowbar.nuspec" `
-        -id "Crowbar" `
-        -version $version `
-        -description "Crowbar is an application testing library for ASP.NET MVC 3 and 4." `
-        -author "Mattias Rydengren" `
-        -licenseUrl "https://github.com/coderesque/crowbar/blob/master/LICENSE" `
-        -projectUrl "https://github.com/coderesque/crowbar" `
-        -tags "crowbar testing"
-        
-    Copy-Item -Destination $nugetDirectory\lib\net40 -LiteralPath `
-        $binariesDirectory\Crowbar.dll, `
-        $binariesDirectory\Crowbar.pdb, `
-        $binariesDirectory\Crowbar.xml 
+    New-Item $path_nuget\lib\net40 -ItemType Directory | Out-Null
+		
+	Copy-Item Crowbar.nuspec $path_nuget
+	
+	# Update version in nuspec.
+    $content = [xml](Get-Content $nuspec)
+	$content.package.metadata.version = $version
+	$content.Save($nuspec)
+		
+	Copy-Item -Destination $path_nuget\lib\net40 -LiteralPath `
+        $path_binaries\Crowbar.dll, `
+        $path_binaries\Crowbar.pdb, `
+        $path_binaries\Crowbar.xml 
     
-    & $toolsDirectory\nuget-cli\nuget.exe pack $nugetDirectory\Crowbar.nuspec -o $nugetDirectory | Out-Null
-    Remove-Item -LiteralPath $nugetDirectory\lib, $nugetDirectory\Crowbar.nuspec -Force -Recurse -ErrorAction SilentlyContinue
+    & $path_tools\nuget-cli\nuget.exe pack $nuspec -o $path_nuget | Out-Null
+    Remove-Item -LiteralPath $path_nuget\lib, $nuspec -Force -Recurse -ErrorAction SilentlyContinue
 }
--- extensions.ps1
+++ extensions.ps1
@@ -31,42 +31,6 @@ using System.Runtime.InteropServices;
         -content $assemblyInfo
 }
 
-function Generate-NuGet-Spec
-{
-    param(
-        [string]$file = $(throw "file is a required parameter."),
-        [string]$id = $(throw "id is a required parameter."),
-        [string]$version = $(throw "version is a required parameter."),
-        [string]$description = $(throw "description is a required parameter."),
-        [string]$author = $(throw "author is a required parameter."),
-        [string]$licenseUrl = $(throw "licenseUrl is a required parameter."),
-        [string]$projectUrl = $(throw "projectUrl is a required parameter."),
-        [string]$tags = $(throw "tags is a required parameter.")
-    )
-
-    $nugetSpec =
-"<?xml version=""1.0""?>
-<package xmlns=""http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd"">
-  <metadata>
-    <id>$id</id>
-    <version>$version</version>
-    <description>$description</description>
-    <authors>$author</authors>
-    <requireLicenseAcceptance>false</requireLicenseAcceptance>
-    <licenseUrl>$licenseUrl</licenseUrl>
-    <projectUrl>$projectUrl</projectUrl>
-    <tags>$tags</tags>
-	<dependencies>
-	  <dependency id=""CsQuery"" version=""[1.3,1.4)"" />
-	</dependencies>
-  </metadata>
-</package>"
-
-    Generate-File-Content `
-        -file $file `
-        -content $nugetSpec
-}
-
 function Generate-File-Content
 {
     param(
