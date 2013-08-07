f7ecd4c5c09859a4a1adb46e9c0929883c7cd942
Mattias Rydengren <mattias.rydengren@coderesque.com>
2013-08-07 19:05:15 +0200

    Added nuspec for Mustache project and updated build script.

--- default.ps1
+++ default.ps1
@@ -1,27 +1,36 @@
 ﻿include .\extensions.ps1
 
 properties {
-    $name                   = "Crowbar"
-    $version                = "0.9.6"
+    $name                       = "Crowbar"
+    $version                    = "0.9.6"
     
     # files that should be part of the nuget.
-    $nuget_package_files    = @( "$name.???" )
+    $nuget_core_package_files     = @( "$name.???" )
+    $nuget_mustache_package_files = @( "$name.Mustache.???" )
 
-    $root                   = Resolve-Path .
+    $root                       = Resolve-Path .
     
     # nuget
-    $nuspec                 = "$name.nuspec"
-    $nuspec_file            = "$root\$nuspec"
+    $nuspec                     = "$name.nuspec"
+    $nuspec_file                = "$root\$nuspec"
+                                
+    # build                     
+    $build_path                 = "$root\build"
+    $build_binaries_path        = "$build_path\binaries\"
+    $build_nuget_path           = "$build_path\nuget"
+                                
+    # source                    
+    $source_path                = "$root\src"
+    $solution_file              = "$source_path\$name.sln"
+                                
+    # nuget                     
+    $nuspec_core                = "$name.nuspec"
+    $nuspec_core_build_file     = "$build_path\nuget\core\$nuspec_core"
+    $nuspec_core_file           = "$root\$nuspec_core"
     
-    # build
-    $build_path             = "$root\build"
-    $build_binaries_path    = "$build_path\binaries\"
-    $build_nuget_path       = "$build_path\nuget"
-    $nuspec_build_file      = "$build_path\nuget\$nuspec"
-    
-    # source
-    $source_path            = "$root\src"
-    $solution_file          = "$source_path\$name.sln"    
+    $nuspec_mustache            = "$name.Mustache.nuspec"
+    $nuspec_mustache_build_file = "$build_path\nuget\mustache\$nuspec_mustache"
+    $nuspec_mustache_file       = "$root\$nuspec_mustache"
 }
 
 Framework "4.0"
@@ -52,18 +61,42 @@ task build -depends clean {
     msbuild $solution_file /p:OutDir=$build_binaries_path /p:Configuration=Release /v:q
 }
 
-task nuget -depends build {
+task nuget-core -depends build {
+    $build_artifacts = "$build_nuget_path\core"
+
     # copy files that should be part of the nuget.
-    New-Item $build_nuget_path\lib\net40 -ItemType Directory | Out-Null
-    $nuget_package_files |% { Copy-Item $build_binaries_path\$_ $build_nuget_path\lib\net40 }
+    New-Item $build_artifacts\lib\net40 -ItemType Directory | Out-Null
+    $nuget_core_package_files |% { Copy-Item $build_binaries_path\$_ $build_artifacts\lib\net40 }
 
     # make a copy of the nuspec making temporary edits possible.
-    Copy-Item $nuspec_file $nuspec_build_file
+    Copy-Item $nuspec_core_file $nuspec_core_build_file
     
 	# update version in the nuspec copy.
-    $content = [xml](Get-Content $nuspec_build_file)
+    $content = [xml](Get-Content $nuspec_core_build_file)
 	$content.package.metadata.version = $version
-	$content.Save($nuspec_build_file)
+	$content.Save($nuspec_core_build_file)
     
-    & "$source_path\.nuget\NuGet.exe" pack $nuspec_build_file -o $build_nuget_path | Out-Null
+    & "$source_path\.nuget\NuGet.exe" pack $nuspec_core_build_file -o $build_artifacts
+}
+
+task nuget-mustache -depends build {
+    $build_artifacts = "$build_nuget_path\mustache"
+
+    # copy files that should be part of the nuget.
+    New-Item $build_artifacts\lib\net40 -ItemType Directory | Out-Null
+    $nuget_mustache_package_files |% { Copy-Item $build_binaries_path\$_ $build_artifacts\lib\net40 }
+
+    # make a copy of the nuspec making temporary edits possible.
+    Copy-Item $nuspec_mustache_file $nuspec_mustache_build_file
+    
+	# update version in the nuspec copy.
+    $content = [xml](Get-Content $nuspec_mustache_build_file)
+	$content.package.metadata.version = $version
+	$content.Save($nuspec_mustache_build_file)
+    
+    & "$source_path\.nuget\NuGet.exe" pack $nuspec_mustache_build_file -o $build_artifacts
+}
+
+task nuget -depends nuget-core, nuget-mustache {
+    # noop
 }
